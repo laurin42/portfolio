33574cd6ce2d1436f4355db678ed43d67e095eaf
laurin42 <laurin.schmidt@yahoo.com>
2025-11-05 14:20:09 +0100

    installed gsap, changed favicon, deleted unneccessary svgs, added accent color in globals.css

--- components/home/HeroClient.tsx
+++ components/home/HeroClient.tsx
@@ -1,14 +1,14 @@
-"use client"
-
+"use client";
 
 export default function HeroClient() {
-    return (
-        <section className="h-[100svh] w-full">
-            <div className="h-full flex flex-col justify-center items-center">
-                <h1>
-                Hello, I am Laurin.
-                </h1>
-            </div>
-        </section>
-    )
+  return (
+    <section className="h-svh w-full">
+      <div className="h-full flex flex-col justify-center items-center">
+        <h1 className="font-funnel text-8xl">
+          Hello<span className="text-accent">, </span>I am Laurin
+          <span className="text-accent">.</span>
+        </h1>
+      </div>
+    </section>
+  );
 }
