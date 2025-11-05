26f22c9a7cda34722f69c51450cdb2d5be73be62
laurin42 <laurin.schmidt@yahoo.com>
2025-11-05 11:42:18 +0100

    project portfolio initial setup

---
+++ components/home/HeroClient.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+
+export default function HeroClient() {
+    return (
+        <section className="h-[100svh] w-full">
+            <div className="h-full flex flex-col justify-center items-center">
+                <h1>
+                Hello, I am Laurin.
+                </h1>
+            </div>
+        </section>
+    )
+}
