91438a37c27727b1fca4d47eb02e2ca7421161e7
laurin42 <laurin.schmidt@yahoo.com>
2025-11-28 12:04:06 +0100

    added motion animation to Contact

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,18 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 export default function Contact() {
   return (
     <>
       <section className="relative min-h-screen flex flex-col justify-center items-center font-funnel">
         <Link href="mailto:lauri.schmidt@proton.me" target="blank">
-          <h2 className="text-5xl sm:text-9xl text-center pb-4 mx-16 sm:mx-0 mb-2  border-b-4 border-accent/64 font-extrabold bg-linear-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent">
+          <motion.h2
+            initial={{ scaleY: 0 }}
+            whileInView={{ scaleY: 1 }}
+            transition={{ duration: 0.2, ease: "easeIn" }}
+            style={{ transformOrigin: "bottom" }}
+            className="text-5xl sm:text-9xl text-center pb-4 mx-16 sm:mx-0 mb-2  border-b-4 border-accent/64 font-extrabold bg-linear-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% bg-clip-text text-transparent"
+          >
             Contact Me Today
-          </h2>
+          </motion.h2>
         </Link>
-        <p className="text-md text-muted-foreground sm:text-3xl pb-4 sm:pb-0 max-w-xl">
+        <motion.p
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 100 }}
+          transition={{ duration: 0.4, delay: 0.4, ease: "easeInOut" }}
+          className="text-md text-muted-foreground sm:text-3xl pb-4 sm:pb-0 max-w-xl"
+        >
           Let&apos;s build something great together
-        </p>
+        </motion.p>
         <div className="flex gap-8 pt-4 sm:pt-16">
           <Link
             href="mailto:laurin.schmidt@proton.me"
